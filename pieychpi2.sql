1ede0af40888368a56d9d1ca55d44bc29ac2abd2
Deomar Torres <djtores102@gmail.com>
2017-10-17 23:13:28 +0800

    Database update lng

--- pieychpi2.sql
+++ pieychpi2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2017 at 07:15 PM
+-- Generation Time: Oct 17, 2017 at 05:10 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.0.23
 
@@ -25,19 +25,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblclient`
+-- Table structure for table `tblaccounts`
 --
 
-CREATE TABLE `tblclient` (
-  `client_id` bigint(20) NOT NULL,
-  `client_firstname` varchar(40) NOT NULL,
-  `client_middlename` varchar(40) NOT NULL,
-  `client_lastname` varchar(40) NOT NULL,
-  `email` varchar(80) NOT NULL,
-  `password` varchar(80) NOT NULL,
-  `client_savings` double NOT NULL,
-  `client_created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `client_updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE `tblaccounts` (
+  `account_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `account_password` varchar(100) NOT NULL,
+  `account_savings` double NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcreditcards`
+--
+
+CREATE TABLE `tblcreditcards` (
+  `card_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `card_password` int(4) NOT NULL,
+  `card_savings` double NOT NULL,
+  `card_created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `card_updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbldeposits`
+--
+
+CREATE TABLE `tbldeposits` (
+  `dep_id` int(11) NOT NULL,
+  `dep_amount` double NOT NULL,
+  `dep_user_id` int(11) NOT NULL,
+  `dep_date` varchar(30) NOT NULL,
+  `dep_time` varchar(30) NOT NULL,
+  `dep_location` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -47,10 +74,43 @@ CREATE TABLE `tblclient` (
 --
 
 CREATE TABLE `tbltransaction` (
-  `tr_id` int(11) NOT NULL,
-  `tr_name` int(11) NOT NULL,
-  `tr_date` int(11) NOT NULL,
-  `tr_time` int(11) NOT NULL
+  `tran_id` int(11) NOT NULL,
+  `tran_type` varchar(100) NOT NULL,
+  `tran_user_id` int(11) NOT NULL,
+  `tran_date` int(11) NOT NULL,
+  `tran_time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblusers`
+--
+
+CREATE TABLE `tblusers` (
+  `user_id` int(11) NOT NULL,
+  `user_fname` varchar(100) NOT NULL,
+  `user_mname` varchar(100) NOT NULL,
+  `user_lname` varchar(100) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_password` varchar(100) NOT NULL,
+  `user_created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblwithdraw`
+--
+
+CREATE TABLE `tblwithdraw` (
+  `with_id` int(11) NOT NULL,
+  `with_amount` int(11) NOT NULL,
+  `with_user_id` int(11) NOT NULL,
+  `with_date` int(11) NOT NULL,
+  `with_time` int(11) NOT NULL,
+  `with_location` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -58,20 +118,62 @@ CREATE TABLE `tbltransaction` (
 --
 
 --
--- Indexes for table `tblclient`
+-- Indexes for table `tblaccounts`
 --
-ALTER TABLE `tblclient`
-  ADD PRIMARY KEY (`client_id`);
+ALTER TABLE `tblaccounts`
+  ADD PRIMARY KEY (`account_id`);
+
+--
+-- Indexes for table `tblcreditcards`
+--
+ALTER TABLE `tblcreditcards`
+  ADD PRIMARY KEY (`card_id`);
+
+--
+-- Indexes for table `tbldeposits`
+--
+ALTER TABLE `tbldeposits`
+  ADD PRIMARY KEY (`dep_id`);
+
+--
+-- Indexes for table `tbltransaction`
+--
+ALTER TABLE `tbltransaction`
+  ADD PRIMARY KEY (`tran_id`);
+
+--
+-- Indexes for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `tblwithdraw`
+--
+ALTER TABLE `tblwithdraw`
+  ADD PRIMARY KEY (`with_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tblclient`
+-- AUTO_INCREMENT for table `tblaccounts`
 --
-ALTER TABLE `tblclient`
-  MODIFY `client_id` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tblaccounts`
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tblcreditcards`
+--
+ALTER TABLE `tblcreditcards`
+  MODIFY `card_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
